dcc7be2d1ec4313a8bc53c0518f0470796296dd7
Grieco Pelisser <grieco.pelisser.analytics@gmail.com>
2024-08-08 21:21:45 +0000

    Rename folders, create the first model

---
+++ models/silver-sap-adventureworks/production/stg_sap__products.sql
@@ -0,0 +1,32 @@
+with
+    products_source as (
+        select
+            cast(productid as string) as product_id
+            , cast(productsubcategoryid as string) as product_subcategory_id
+            , cast(productmodelid as string) as product_model_id
+            , cast(name as string) as product_name
+            , cast(productnumber as string) as product_number
+            --, cast(makeflag as boolean) as is_product_manufactured
+            --, color 
+            --, safetystocklevel 
+            --, reorderpoint
+            , cast(standardcost as decimal) as product_standardcost
+            --, listprice 
+            , cast(size as string) as product_size
+            , cast(sizeunitmeasurecode as string) as size_unitmeasure_code
+            , cast(weightunitmeasurecode as string) as weight_unitmeasure_code
+            , cast(weight  as decimal) as product_weight
+            , cast(daystomanufacture  as int) as days_to_manufacture
+            , cast(productline as string) as product_line
+            , cast(class  as string) as product_class
+            , cast(style as string) as product_style
+            , date(sellstartdate) as product_sellstart_date
+            , date(sellenddate) as product_sellend_date
+            , cast(finishedgoodsflag as boolean) as is_product_salable
+            --, discontinueddate
+            --, rowguid
+            --, modifieddate 
+        from {{ source('sap', 'product') }}
+    )
+select *
+from products_source
