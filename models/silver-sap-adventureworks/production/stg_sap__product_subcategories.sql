0ab86f9c7fe22052bab4e2b06bdc528a14313751
Grieco Pelisser <grieco.pelisser.analytics@gmail.com>
2024-08-10 01:30:57 +0000

    Created staging tables for the production schema.

---
+++ models/silver-sap-adventureworks/production/stg_sap__product_subcategories.sql
@@ -0,0 +1,10 @@
+with
+    product_subcategories_source as (
+        select
+            cast(productsubcategoryid as string) as product_subcategory_id
+            , cast(productcategoryid as string) as product_category_id
+            , cast(name as string) as product_subcategory_name 
+        from {{ source('sap', 'productsubcategory') }}
+    )
+select *
+from product_subcategories_source
