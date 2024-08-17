b85f972dd3e10f810c97a408f10461d5179ed171
Grieco Pelisser <grieco.pelisser.analytics@gmail.com>
2024-08-17 21:27:09 +0000

    Updated the reference for the dim_sales_persons table.

--- models/gold/dim_sales_persons.sql
+++ models/gold/dim_sales_persons.sql
@@ -1,41 +1,27 @@
 with
-    stg_sales_persons as (
+    int_sales_persons as (
         select *
-        from {{ ref('stg_sap__sales_persons') }}
+        from {{ ref('int_sales_persons') }}
     )
 
-    , int_persons_addresses as(
-        select *
-        from {{ ref('int_persons_addresses') }}
-    )
-
-    , stg_sales_territories as (
-        select *
-        from {{ ref('stg_sap__sales_territories')}}
-    )
-
-    , join_tables as (
+    , select_columns as (
         select
-            stg_sales_persons.sales_person_id
-            , int_persons_addresses.full_name
-            , int_persons_addresses.person_type
-            , stg_sales_persons.commission_pct 
-            , stg_sales_persons.territory_id
-            , stg_sales_territories.territory_name
-            , stg_sales_territories.country_region_code
-            , stg_sales_territories.territory_group
-        from stg_sales_persons
-        left join int_persons_addresses on
-            stg_sales_persons.sales_person_id = int_persons_addresses.person_id
-        left join stg_sales_territories on
-            stg_sales_persons.territory_id = stg_sales_territories.territory_id
+            sales_person_id
+            , full_name
+            , person_type
+            , commission_pct 
+            , territory_id
+            , territory_name
+            , country_region_code
+            , territory_group
+        from int_sales_persons
         )
 
     , create_sk as (
         select
             {{ dbt_utils.generate_surrogate_key(['sales_person_id']) }} as sales_person_sk
             , *
-        from join_tables
+        from select_columns
     )
 
 select *
